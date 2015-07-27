6f7be96a454525fc5ed8abcad852791e37be01b1
Marc Fortier <Marc.Fortier@statcan.gc.ca>
2015-07-27 15:57:20 -0400

    forgot to declare the cubre_create_name validator in plugins.py

--- ckanext/stcndm/plugins.py
+++ ckanext/stcndm/plugins.py
@@ -72,6 +72,7 @@ class STCNDMPlugin(p.SingletonPlugin):
             "subject_create_name": validators.subject_create_name,
             "geodescriptor_create_name": validators.geodescriptor_create_name,
             "dimension_member_create_name": validators.dimension_member_create_name,
+            "cube_create_name": validators.cube_create_name,
             "imdb_create_name": validators.imdb_create_name,
             "ndm_tag_name_validator": validators.ndm_tag_name_validator,
         }
