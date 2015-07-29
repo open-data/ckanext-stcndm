e4a6fc01f7f00cc7794717e9895607cc846834bb
Tyler Kennedy <tk@tkte.ch>
2015-07-29 17:05:09 -0500

    Refactor GetSubject, remove unused imports.

--- ckanext/stcndm/logic/subjects.py
+++ ckanext/stcndm/logic/subjects.py
@@ -1,23 +1,13 @@
-__author__ = 'Statistics Canada'
-
+#!/usr/bin/env python
+# encoding: utf-8
 import ckanapi
-import ckan.logic as logic
-import ckan.plugins.toolkit as toolkit
 
-_get_or_bust = logic.get_or_bust
-# noinspection PyUnresolvedReferences
-_get_action = toolkit.get_action
-# noinspection PyUnresolvedReferences
-_ValidationError = toolkit.ValidationError
-# noinspection PyUnresolvedReferences
-_NotFound = toolkit.ObjectNotFound
-# noinspection PyUnresolvedReferences
-_NotAuthorized = toolkit.NotAuthorized
+from ckan.logic import get_or_bust, side_effect_free
+from ckan.plugins.toolkit import ValidationError, ObjectNotFound
 
 
-@logic.side_effect_free  # Allow GET access
+@side_effect_free  # Allow GET access
 def get_subject(context, data_dict):
-    # noinspection PyUnresolvedReferences
     """
     :param: subjectCode: Subject Code (i.e. 13)
     :type: str
@@ -25,46 +15,36 @@ def get_subject(context, data_dict):
     :return: English, French and code values for given subjectCode
     :rtype: dict
     """
-
-    subject_code = data_dict['subjectCode']
-
-    q = {
-        'q': 'tmtaxsubjcode_bi_tmtxtm:{subject_code}'.format(
-            subject_code=subject_code
+    lc = ckanapi.LocalCKAN(context=context)
+    subject_code = get_or_bust(data_dict, 'subjectCode')
+    response = lc.action.package_search(
+        q='dataset_type:codeset AND extras_codeset_value:{value}'.format(
+            value=subject_code
         )
-    }
+    )
 
-    result = _get_action('package_search')(context, q)
-
-    count = result['count']
-
-    if count == 0:
-        raise _NotFound(
-            'No subject found with subject code {0}'.format(subject_code)
-        )
-    if count > 1:
-        raise _ValidationError(
-            'Duplicate SubjectCodes have been entered in CKAN {0}'.format(
-                subject_code
+    if not response['count']:
+        raise ObjectNotFound(
+            'No subject found with subject code {subject_code}'.format(
+                subject_code=subject_code
             )
         )
+    elif response['count'] > 1:
+        raise ValidationError((
+            'Duplicate SubjectCodes have been entered '
+            'in CKAN {subject_code}'
+        ).format(
+            subject_code
+        ))
     else:
-        desired_extras = [
-            'tmtaxsubj_en_tmtxtm',
-            'tmtaxsubj_fr_tmtxtm',
-            'tmtaxsubjcode_bi_tmtxtm'
-        ]
-
-        output = {}
-
-        for extra in result['results'][0]['extras']:
-            if extra['key'] in desired_extras:
-                output[extra['key']] = extra['value']
-
-        return output
+        r = response['results'][0]
+        return {
+            'title': r['title'],
+            'subject_code': r['codeset_value']
+        }
 
 
-@logic.side_effect_free
+@side_effect_free
 def get_top_level_subject_list(context, data_dict):
     """
     Return a list of all top level subject codes and titles.
