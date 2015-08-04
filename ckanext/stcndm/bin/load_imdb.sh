b4c4084379adab69b502c137e89fbecf165c6c6e
Marc Fortier <Marc.Fortier@statcan.gc.ca>
2015-08-04 16:29:47 -0400

    cleaning up load codesets

--- ckanext/stcndm/bin/load_imdb.sh
+++ ckanext/stcndm/bin/load_imdb.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-#curl http://ndmckanq1.stcpaz.statcan.gc.ca:8282/solr/ndm_core_dev/query?q=organization:maimdb\&rows=70000 | \
-#  jq '.response.docs' | \
 python massage_imdb.py | \
 #  jq '.' | \
   ckanapi load datasets $@
