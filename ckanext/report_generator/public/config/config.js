c25d20430464b4477e1fc78fa1aa0cad3e98435c
Laurent Goderre <laurent.goderre@gmail.com>
2015-09-03 10:41:52 -0400

    Replaced the config file with a Jinja generated script for Solr & CKAN config

--- ckanext/report_generator/public/config/config.js
+++
@@ -1,9 +0,0 @@
-(function(window, angular) {'use strict';
-
-    angular.module('services.config', [])
-        .constant('configuration', {
-            ckanInstance: 'http://ndmckanq1.stcpaz.statcan.gc.ca/zj',
-            solrCore: 'http://ndmckanq1.stcpaz.statcan.gc.ca/so04'
-        });
-
-})(window, angular);
