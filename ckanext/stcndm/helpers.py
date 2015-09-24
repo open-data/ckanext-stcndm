1654ad73527c11b6dab6a3e666018c2c5ae75944
marcfor <Marc.Fortier@statcan.gc.ca>
2015-09-24 07:37:43 -0400

    Fix for problem tickled by looking up subject_old labels

--- ckanext/stcndm/helpers.py
+++ ckanext/stcndm/helpers.py
@@ -276,7 +276,7 @@ def lookup_label(field_name, field_value, lookup_type):
 
     default = {u'en': 'label for ' + field_value, u'fr': 'description de ' + field_value, u'found': False}
 
-    if not field_value:
+    if not field_value or not lookup_type:
         return default
 
     if lookup_type == 'preset':
