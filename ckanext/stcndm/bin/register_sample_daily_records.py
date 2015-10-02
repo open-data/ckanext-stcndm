3fe268c70c3d71b6f1694ad4eae626a238ed0691
Marc Fortier <Marc.Fortier@statcan.gc.ca>
2015-10-02 09:01:15 -0400

    scripts to update sample daily records

--- ckanext/stcndm/bin/register_sample_daily_records.py
+++ ckanext/stcndm/bin/register_sample_daily_records.py
@@ -7,8 +7,8 @@ import ConfigParser
 parser = ConfigParser.SafeConfigParser()
 parser.read("./ckanparameters.config")
 
-API_KEY = parser.get("ckan", "api_key")
-BASE_URL = parser.get("ckan", "base_url")
+API_KEY = parser.get("ckandev", "api_key")
+BASE_URL = parser.get("ckandev", "base_url")
 
 
 def get_daterange(start_date, end_date):
