34015b626d2258c5f4caae1a5724dc0e5aa98fc0
Laurent Goderre <laurent.goderre@gmail.com>
2015-10-22 14:23:04 -0400

    Report Generator: Added support for proxied solr and add better error reporting

--- ckanext/report_generator/controller.py
+++ ckanext/report_generator/controller.py
@@ -4,9 +4,12 @@ from ckan.lib.base import BaseController, config
 
 class ReportGeneratorController(BaseController):
 
-    c.solr_url = config.get('solr_url',
+    SOLR_URL = config.get('solr_url',
             'http://127.0.0.1:8983/solr/ckan')
-    c.ckan_url = config.get('ckan.site_url', '')
+    CKAN_URL = config.get('ckan.site_url', '')
 
     def index(self):
+        c.solr_url = self.SOLR_URL
+        c.ckan_url = self.CKAN_URL
+
         return p.toolkit.render('ckanext/report_generator/index.html')
