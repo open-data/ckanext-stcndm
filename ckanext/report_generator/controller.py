96ff64b83a9cd65054ad30ed230ee2bc2663c999
Laurent Goderre <laurent.goderre@gmail.com>
2015-10-22 10:17:00 -0400

    Report Generator: Avoid reading the config on each request

--- ckanext/report_generator/controller.py
+++ ckanext/report_generator/controller.py
@@ -1,11 +1,12 @@
 import ckan.plugins as p
+from ckan.plugins.toolkit import c
 from ckan.lib.base import BaseController, config
 
 class ReportGeneratorController(BaseController):
 
+    c.solr_url = config.get('solr_url',
+            'http://127.0.0.1:8983/solr/ckan')
+    c.ckan_url = config.get('ckan.site_url', '')
+
     def index(self):
-        from pylons import tmpl_context as c
-        c.solr_url = config.get('solr_url',
-                '/solr/ckan')
-        c.ckan_url = config.get('ckan.site_url', '')
         return p.toolkit.render('ckanext/report_generator/index.html')
