74beedcbc3b5f1d2ac6a3d9ee685336a3c16f939
Laurent Goderre <laurent.goderre@gmail.com>
2015-10-27 09:23:39 -0400

    Report Generator; Fixed field sorting and filter results by site_id

--- ckanext/report_generator/controller.py
+++ ckanext/report_generator/controller.py
@@ -7,9 +7,11 @@ class ReportGeneratorController(BaseController):
     SOLR_URL = config.get('solr_url',
             'http://127.0.0.1:8983/solr/ckan')
     CKAN_URL = config.get('ckan.site_url', '')
+    SITE_ID = config.get('ckan.site_id', '')
 
     def index(self):
         c.solr_url = self.SOLR_URL
         c.ckan_url = self.CKAN_URL
+        c.site_id = self.SITE_ID
 
         return p.toolkit.render('ckanext/report_generator/index.html')
