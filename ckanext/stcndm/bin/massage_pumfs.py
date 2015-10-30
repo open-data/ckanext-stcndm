eb94a8826104f41cf443735304a3039e33aff177
Marc Fortier <Marc.Fortier@statcan.gc.ca>
2015-10-30 14:49:35 -0400

    deprecation of releases
    moved fields from release schema to product schemas
    updated massage scripts
    incorporated issn_number and load_to_olc values provided by jackie

--- ckanext/stcndm/bin/massage_pumfs.py
+++ ckanext/stcndm/bin/massage_pumfs.py
@@ -27,15 +27,15 @@ while i < n:
             ).lower()
         print json.dumps(product_out)
 
-        if product_out['product_id_new'] in release_dict:
-            release_dict[product_out['product_id_new']] += 1
-        else:
-            release_dict[product_out['product_id_new']] = 1
-        line['release_id'] = unicode(release_dict[product_out['product_id_new']])
-
-        release_out = do_release(line)
-        print json.dumps(release_out)
+        # if product_out['product_id_new'] in release_dict:
+        #     release_dict[product_out['product_id_new']] += 1
+        # else:
+        #     release_dict[product_out['product_id_new']] = 1
+        # line['release_id'] = unicode(release_dict[product_out['product_id_new']])
+        #
+        # release_out = do_release(line)
+        # print json.dumps(release_out)
 
         format_out = do_format(line)
-        format_out['release_slug'] = release_out['name']
+        # format_out['release_slug'] = release_out['name']
         print json.dumps(format_out)
