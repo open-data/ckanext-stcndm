c4ddaa3c14dabf3b3feb714bbce8d4113d76454e
Matt Leduc <matt.leduc@statcan.gc.ca>
2015-11-05 04:29:19 -0500

    script to migrate required release fields to parent records in order to deprecate releases

---
+++ ckanext/stcndm/bin/migrate_release_fields_to_parent_records.py
@@ -0,0 +1,71 @@
+import ckanapi
+import ConfigParser
+
+parser = ConfigParser.SafeConfigParser()
+parser.read("./ckanparameters.config")
+
+API_KEY = parser.get("ckan", "api_key")
+BASE_URL = parser.get("ckan", "base_url")
+
+
+def main():
+    rc = ckanapi.RemoteCKAN(BASE_URL,
+                            apikey=API_KEY)
+
+    for dataset_type in ['article',
+                         'publication',
+                         'cube',
+                         'view',
+                         'indicator']:
+
+        # TODO: all cubes and many articles are missing some of these fields in their releases
+
+        # TODO: Why do formats not have parent_id/release_slug in the schema?
+
+        q = u'dataset_type:{dataset_type}'.format(dataset_type=dataset_type)
+
+        i = 0
+        n = 1
+
+        while i < n:
+            query_results = rc.action.package_search(
+                q=q,
+                rows=1000,
+                start=i*1000
+            )
+            n = query_results['count'] / 1000.0
+            i += 1
+
+            for record in query_results['results']:
+
+                release = rc.action.package_search(
+                    q='dataset_type:release AND parent_id:{product_id_new}'.format(
+                        product_id_new=record['product_id_new']
+                    ),
+                    rows=1,
+                    sort='release_date desc'
+                )
+
+                if release['count'] > 0:
+                    latest_release = release['results'][0]
+                    try:
+                        record['last_release_date'] = latest_release['release_date']
+                    except KeyError:
+                        print '{record}: missing last_release_date'.format(
+                            record=record['name']
+                        )
+                    for field in ['reference_period',
+                                  'display_code',
+                                  'issue_number']:
+                        try:
+                            record[field] = latest_release[field]
+                        except KeyError:
+                            print '{record}: missing {field}'.format(
+                                record=record['name'],
+                                field=field
+                            )
+
+                    rc.action.package_update(**record)
+
+if __name__ == "__main__":
+    main()
