2bda007e7ed908539206e192d71af287b2a193c8
Ross Clarke <ross.clarke@canada.ca>
2015-12-11 15:09:36 -0500

    Selenium Test Cases for reads.  Still needs better intergration into CKAN testing standard.

--- ckanext/stcndm/testselenium/test_register.py
+++ ckanext/stcndm/testselenium/test_register.py
@@ -13,10 +13,11 @@ class TestAuthNav(unittest.TestCase):
 
     def test_register(self):
         phantom.get(setup_test.ckan_base)
-        phantom.find_element_by_id('field-username').send_keys(setup_test.ckan_reg_user)
-        phantom.find_element_by_id('field-fullname').send_keys(setup_test.ckan_reg_user)
-        phantom.find_element_by_id('field-email').send_keys(setup_test.ckan_reg_email)
-        phantom.find_element_by_id('field-password').send_keys(setup_test.ckan_reg_pass)
-        phantom.find_element_by_id('field-confirm-password').send_keys(setup_test.ckan_reg_pass)
+        phantom.find_element_by_link_text('Register').click()
+        phantom.find_element_by_name('name').send_keys(setup_test.ckan_reg_user)
+        phantom.find_element_by_name('fullname').send_keys(setup_test.ckan_reg_user)
+        phantom.find_element_by_name('email').send_keys(setup_test.ckan_reg_email)
+        phantom.find_element_by_name('password1').send_keys(setup_test.ckan_reg_pass)
+        phantom.find_element_by_name('password2').send_keys(setup_test.ckan_reg_pass)
         phantom.find_element_by_name('save').click()
         assert_equals(phantom.title, 'Dashboard - CKAN')
