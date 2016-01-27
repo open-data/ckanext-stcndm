9060ccb05b666264d31e5170f8c047f09e34bdae
Ross Clarke <ross.clarke@canada.ca>
2016-01-27 07:28:30 -0500

    Update API tests.

--- ckanext/stcndm/testfunctional/functional_base.py
+++ ckanext/stcndm/testfunctional/functional_base.py
@@ -7,7 +7,10 @@ ckan_user = os.environ['ckan_user']
 ckan_password = os.environ['ckan_password']
 ckan_api = os.environ['ckan_api']
 ckan_url = 'http://127.0.0.1:5000'
-headers = {'Authorization': 'token {0}'.format(ckan_api)}
+headers = {
+    'Authorization': 'token {0}'.format(ckan_api),
+    'content-type': 'application/json'
+}
 
 phantom = PhantomJS(service_args=['--proxy-type=none', '--load-images=false'])
 phantom.set_window_size(1024, 768)
@@ -21,7 +24,8 @@ def login_or_pass(driver):
         driver.find_element_by_link_text('Log in').click()
         driver.find_element_by_name('login').send_keys(ckan_user)
         driver.find_element_by_name('password').send_keys(ckan_password)
-        driver.find_element_by_xpath('//*[@id="wb-auto-1"]/div/div/div/form/div[4]/button').click()
+        driver.find_element_by_xpath(
+            '//*[@id="wb-auto-1"]/div/div/div/form/div[4]/button').click()
         driver.find_element_by_class_name('username')
     else:
         return driver
